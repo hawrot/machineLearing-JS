e27d62d64a63df4b5c2d002317d220bf1f8db7b2
hawrot <mhawro200@caledonian.ac.uk>
2020-04-26 00:03:29 +0100

    All logic to score.js

---
+++ section2/testing-algorithm.js
@@ -0,0 +1,26 @@
+const outputs = [];
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+    outputs.push([dropPosition, bounciness, size, bucketLabel]);
+
+}
+
+const predictionPoint = 300;
+const k = 3;
+
+function runAnalysis() {
+ const bucket =   _.chain(outputs)
+        .map(row => [distance(row[0]), row[3]])
+        .sortBy(row => row[0])
+        .slice(0,k)
+        .countBy(row => row[1])
+        .toPairs()
+        .sortBy(row => row[1])
+        .last()
+        .first()
+console.log('Point will fall into bucket:' + bucket);
+}
+
+function distance(point) {
+    return Math.abs(point - predictionPoint);
+}
