a66ad3f00e095a27abd08f23c5070c50a0594c3f
hawrot <mhawro200@caledonian.ac.uk>
2020-04-25 23:37:00 +0100

    Add helpers-images

--- section2/implemeting-Knn.js
+++ section2/implemeting-Knn.js
@@ -1,3 +1,11 @@
+var _ = require('lodash');
+var _ = require('lodash/core');
+var fp = require('lodash/fp');
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
+
 const outputs =
     [
         [10,.5, 16,1],
@@ -5,3 +13,13 @@ const outputs =
         [350,.5,16,4],
         [600, .5, 16,5]
     ];
+
+const predictionPoint = 300;
+
+function distance(point){
+    return Math.abs(point - predictionPoint);
+}
+
+//Getting the distance and bucket number
+_.chain(outputs)
+.map(row => [distance(row[0]), row[3]])
