49056ed29c7baca2a7dddfa65a0120bfa72d8ee8
hawrot <mhawro200@caledonian.ac.uk>
2020-04-27 21:52:07 +0100

    Create helper function

--- plinko/score.js
+++ plinko/score.js
@@ -9,7 +9,17 @@ const predictionPoint = 300;
 const k = 3;
 
 function runAnalysis() {
-  const bucket =   _.chain(outputs)
+  const bucket =
+
+  console.log('Point will fall into bucket: ' + bucket);
+}
+
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
+
+function knn(){
+return  _.chain(outputs)
       .map(row => [distance(row[0]), row[3]])
       .sortBy(row => row[0])
       .slice(0,k)
@@ -18,13 +28,9 @@ function runAnalysis() {
       .sortBy(row => row[1])
       .last()
       .first()
-  console.log('Point will fall into bucket: ' + bucket);
+      .parseInt()
+      .value();
 }
-
-function distance(point) {
-  return Math.abs(point - predictionPoint);
-}
-
 function splitDataset(data, testCount){
   const shuffled = _.shuffle(data);
 
