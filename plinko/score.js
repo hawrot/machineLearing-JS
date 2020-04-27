a44bc6886a3ac31db49e4f3151a5f9a58427c440
hawrot <mhawro200@caledonian.ac.uk>
2020-04-27 22:03:16 +0100

    Display prediction with actual result

--- plinko/score.js
+++ plinko/score.js
@@ -11,6 +11,11 @@ const k = 3;
 function runAnalysis() {
   const [testSet, trainingSet] = splitDataset(outputs, 10);
 
+  for (let i = 0; i< testSet.length; i++){
+   const bucket =  knn(trainingSet, testSet[i][0]);
+    console.log(bucket, testSet[i][3]);
+  }
+
 }
 
 
