977e82e6a08f82d6955a697df3f4d27cd92f6627
hawrot <mhawro200@caledonian.ac.uk>
2020-06-06 19:26:21 +0100

    Load csv file and set 10 records for testing purpose

---
+++ knn-tf/index.js
@@ -0,0 +1,10 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfs');
+
+const loadCSV = require('./load-csv');
+
+loadCSV('kc_house_data.csv', {
+    shuffle: true,
+    splitTest: 10
+});
+
