afff5db609b554569e69d599181adcaf3ebfd04d
hawrot <mhawro200@caledonian.ac.uk>
2020-06-06 19:39:55 +0100

    Create knn function

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,10 +1,28 @@
 require('@tensorflow/tfjs-node');
-const tf = require('@tensorflow/tfs');
+const tf = require('@tensorflow/tfjs');
 
 const loadCSV = require('./load-csv');
 
-loadCSV('kc_house_data.csv', {
+function knn(features, labels, predictionPoint, k) {
+   return features
+        .sub(predictionPoint)
+        .pow(2)
+        .sum(1)
+        .pow(0.5)
+        .expandDims(1)
+        .concat(labels, 1)
+        .unstack()
+        .sort((a,b) => a.get(0) > b.get(0) ? 1 : -1)
+        .slice(0, k)
+        .reduce((acc, pair) => acc + pair.get(1), 0) /k;
+}
+
+let { features, labels, testFeatures, testLabels } =  loadCSV('kc_house_data.csv', {
     shuffle: true,
-    splitTest: 10
+    splitTest: 10,
+    dataColumns: ['lat', 'long'],
+    labelColumns: ['price']
 });
+
+
 
