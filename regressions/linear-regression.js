5efe809355e6f129815b20ad0d8460342bc5b15d
hawrot <mhawro200@caledonian.ac.uk>
2020-06-07 21:13:17 +0100

    Add gradientDescent function

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -1,11 +1,12 @@
 const tf = require('@tensorflow/tfjs');
+const _ = require('lodash');
 
 class LinearRegression {
     constructor(features, labels, options) {
         this.features = features;
         this.labels = labels;
 
-        this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
+        this.options = Object.assign({learningRate: 0.1, iterations: 1000}, options);
         this.m = 0;
         this.b = 0;
     }
@@ -13,17 +14,21 @@ class LinearRegression {
 }
 
 gradientDescent = () => {
+    const currentGuessesForMPG = this.features.map(row => {
+        return this.m * row[0] + this.b;
+    });
 
+    const bSlope = _.sum(currentGuessesForMPG.map((guess, index) => {
+        return guess - this.labels[index][0];
+    })) * 2 / this.features.length;
 }
 
-function train(){
-    for(let i = 0; i < this.options.iterations; i++){
+function train() {
+    for (let i = 0; i < this.options.iterations; i++) {
         this.gradientDescent();
     }
 
 }
-
-
 
 
 module.exports = LinearRegression;
