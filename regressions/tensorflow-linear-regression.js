0be86e8afcf39c8850b83ff15419fb24590c4f6a
hawrot <mhawro200@caledonian.ac.uk>
2020-06-08 21:46:49 +0100

    Reduce the code by creating the common method

--- regressions/tensorflow-linear-regression.js
+++ regressions/tensorflow-linear-regression.js
@@ -3,10 +3,10 @@ const _ = require('lodash');
 
 class LinearRegression {
     constructor(features, labels, options) {
-        this.features = tf.tensor(features);
+        this.features = this.processFeatures(features);
         this.labels = tf.tensor(labels);
 
-        this.features = tf.ones([this.features.shape[0], 1]).concat(this.features, 1);
+
 
         this.options = Object.assign({learningRate: 0.1, iterations: 1000}, options);
 
@@ -34,10 +34,10 @@ class LinearRegression {
     }
 
     test(testFeatures, testLabels) {
-        testFeatures = tf.tensor(testFeatures);
+        testFeatures = this.processFeatures(testFeatures);
         testLabels = tf.tensor(testLabels);
 
-        testFeatures = tf.ones([testFeatures.shape[0], 1]).concat(testFeatures, 1);
+
         const predictions = testFeatures.matMul(this.weights);
 
 
