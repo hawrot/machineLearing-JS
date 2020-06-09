bb042ad10d59e40e2afb95e0241e797d0fe3ed46
hawrot <mhawro200@caledonian.ac.uk>
2020-06-09 23:34:24 +0100

    Assign the decision boundry

--- logistic-regression/logistic/logistic-regression.js
+++ logistic-regression/logistic/logistic-regression.js
@@ -8,7 +8,7 @@ class LogisticRegression {
         this.mseHistory = [];
 
 
-        this.options = Object.assign({learningRate: 0.1, iterations: 1000}, options);
+        this.options = Object.assign({learningRate: 0.1, iterations: 1000, decisionBoundry: 0.5}, options);
 
         this.weights = tf.zeros([this.features.shape[1], 1]);
     }
@@ -49,7 +49,7 @@ class LogisticRegression {
     }
 
     test(testFeatures, testLabels) {
-      const predictions = this.predict(testFeatures).round();
+      const predictions = this.predict(testFeatures);
       testLabels = tf.tensor(testLabels);
 
       const incorrect = predictions.sub(testLabels).abs().sum().get();
@@ -59,7 +59,7 @@ class LogisticRegression {
     }
 
     predict(observations){
-        return  this.processFeatures(observations).matMul(this.weights).sigmoid();
+        return  this.processFeatures(observations).matMul(this.weights).sigmoid().greater(this.options.decisionBoundry).cast('float32');
     }
 
     processFeatures(features) {
